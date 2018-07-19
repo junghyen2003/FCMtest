7a6a90ac10c532582f25304168a41f9a58ca5969
seochangwook <scw05313315@gmail.com>
2018-07-20 02:12:14 +0900

    Android 6.0 Version Update(Runtime Permission Check)

--- app/src/main/java/com/example/seo/fcmtestapp/MainActivity.java
+++ app/src/main/java/com/example/seo/fcmtestapp/MainActivity.java
@@ -1,24 +1,83 @@
 package com.example.seo.fcmtestapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.Manifest;
 
 import com.example.seo.fcmtestapp.manager.datamanager.PropertyManager;
-import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_PERMISSION_CONTACTS=1;
+
+    Button testbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setBadgeCountInit();
+
+        testbutton = (Button)findViewById(R.id.testbutton);
+
+        testbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showContactsPermission(); //런타임 권한체크//
+            }
+        });
+    }
+
+    private void showContactsPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                showExplanation("Permission Needed", "Rationale", Manifest.permission.READ_CONTACTS, REQUEST_PERMISSION_CONTACTS); //권한필요 설명//
+            } else {
+                requestPermission(Manifest.permission.READ_CONTACTS, REQUEST_PERMISSION_CONTACTS);
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "Permission (already) Granted!", Toast.LENGTH_SHORT).show();
+
+            //이후 작업진행//
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CONTACTS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void showExplanation(String title, String message, final String permission, final int permissionRequestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        requestPermission(permission, permissionRequestCode); //권한 재요청//
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void requestPermission(String permissionName, int permissionRequestCode) {
+        ActivityCompat.requestPermissions(this, new String[]{permissionName}, permissionRequestCode); //권한요청 다이얼로그//
     }
 
     private void setBadgeCountInit() {
